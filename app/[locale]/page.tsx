3c24f60b681ca6b5bd3060d13fd2dc07466136ec
Mo <mo.zaqout@gmail.com>
2026-07-24 21:35:38 +0200

    test IRS revalidate home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,8 @@ import { PropertyExplorer } from '@/components/property-explorer'
 import { getProperties } from '@/lib/properties-service'
 import { setRequestLocale } from 'next-intl/server'
 
+export const revalidate = 60
+
 export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params
   setRequestLocale(locale)
